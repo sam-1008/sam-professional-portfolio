ff5addc6c0b070b64187b43d99ee4bf700ac0084
Sam <tanafrancasam@gmail.com>
2026-05-08 20:52:40 +0800

    Update constants.ts
    Updated Resume URL

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@
 
 import { Project, Experience, Education, Certification, Badge, DigitalCertificate } from './types';
 
-export const RESUME_URL = "https://drive.google.com/file/d/1Go2VmTeN98pAvNAVERJyuY45J4o92ywn/view?usp=sharing";
+export const RESUME_URL = "https://drive.google.com/file/d/15bsKdAFM8SY-HSvUBsyzeHwY9wWsAUgA/view?usp=sharing";
 import edulmsImage from './images/edulms.png';
 import fintrackImage from './images/fintrack.png';
 import fuelintelImage from './images/fuelintel.png';
